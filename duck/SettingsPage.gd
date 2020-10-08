46df68482bc92ae8aec2b1bf35f0bfe006595018
Jon Ring <jonnyring@gmail.com>
2020-10-07 23:22:28 -0400

    Fix difficulty bug

--- duck/SettingsPage.gd
+++ duck/SettingsPage.gd
@@ -1,27 +1,16 @@
 extends ColorRect
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/GridContainer/DifficultyDropdown.add_item("Newly Hatched", 0)
 	$MarginContainer/VBoxContainer/GridContainer/DifficultyDropdown.add_item("Quack Shot", 1)
-	$MarginContainer/VBoxContainer/GridContainer/DifficultyDropdown.add_item("Yippie Ki Yay, Rubber Ducker!", 20)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	$MarginContainer/VBoxContainer/GridContainer/DifficultyDropdown.add_item("Yippie Ki Yay, Rubber Ducker!", 2)
 
 func _on_FullscreenCheckBox_toggled(button_pressed):
 	OS.window_fullscreen = button_pressed
 
-
 func _on_DifficultyDropdown_item_selected(ID):
 	GlobalSettings.difficulty = ID
-
 
 func _on_HSlider_value_changed(value):
 	GlobalSettings.mouseSensitivity = value
